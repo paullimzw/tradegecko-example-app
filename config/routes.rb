faddfe563e36e160c8c5ff61679ea53d517d21fa
Bradley Priest <bradleypriest@gmail.com>
2013-04-02 23:55:22 +0800

    update rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 OauthClient::Application.routes.draw do
-  match '/auth/:provider/callback', to: 'sessions#create'
-  match '/signout', to: 'sessions#destroy', as: 'signout'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy', as: 'signout'
 
   resources :tasks
   root to: 'tasks#index'
