fec04e11f227978b972ae84377591020ca865484
Bradley Priest <bradleypriest@gmail.com>
2013-04-12 12:31:22 +0800

    Update omniauth-tradegecko

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
-require File.expand_path('lib/omniauth/strategies/tradegecko', Rails.root)
-
+require 'omniauth-tradegecko'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :tradegecko, ENV["OAUTH_ID"], ENV["OAUTH_SECRET"]
 end
