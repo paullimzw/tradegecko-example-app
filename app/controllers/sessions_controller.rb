b764936e6ce695d7572fcfd0f7728b0e4722c5c9
Bradley Priest <bradleypriest@gmail.com>
2013-04-14 23:23:51 +0800

    use refresh token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,9 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    token = OAuth2::AccessToken.new(oauth_client, auth["credentials"]["token"], auth["credentials"].slice("refresh_token", "expires_at"))
     session[:user_id] = user.id
-    session[:access_token] = auth["credentials"]["token"]
+    set_session_from_access_token(token)
     redirect_to root_url
   end
 
@@ -11,8 +12,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
-    session[:access_token] = nil
+    clear_session
     redirect_to root_url
   end
 end
