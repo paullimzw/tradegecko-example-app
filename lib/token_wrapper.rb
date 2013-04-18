789e126d6b8444c4dc837abac6b2e617e0ba0cdb
Bradley Priest <bradleypriest@gmail.com>
2013-04-18 18:35:30 +0800

    Add token_wrapper to automatically reauthenticate

---
+++ lib/token_wrapper.rb
@@ -0,0 +1,20 @@
+class TokenWrapper
+  attr_reader :token
+
+  def initialize(token, &callback)
+    @token    = token
+    @callback = callback
+  end
+
+  def method_missing(method, *args, &block)
+    token.send(method, *args, &block)
+  rescue  OAuth2::Error => exception
+    @token = token.refresh!
+    @callback.call(token)
+    token.send(method, *args, &block)
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    token.respond_to?(method_name, include_private) || super
+  end
+end
