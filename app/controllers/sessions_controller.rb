1eafb33c7e9f2ee26629e922c136c635f726b834
Bradley Priest <bradleypriest@gmail.com>
2013-04-19 15:59:15 +0800

    clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     token = OAuth2::AccessToken.new(oauth_client, auth["credentials"]["token"], auth["credentials"].slice("refresh_token", "expires_at"))
     session[:user_id] = user.id
@@ -8,11 +7,13 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 
-  def show
-  end
-
   def destroy
     clear_session
     redirect_to root_url
+  end
+
+private
+  def auth
+    request.env["omniauth.auth"]
   end
 end
