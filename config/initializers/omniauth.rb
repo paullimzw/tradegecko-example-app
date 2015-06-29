1e43a112efc2c51727b94040e66e84359e5f24fd
Paul Lim <paullimzw@gmail.com>
2015-06-29 20:00:01 +0800

    Omniauth Password & Secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 require 'omniauth-tradegecko'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :tradegecko, ENV["OAUTH_ID"], ENV["OAUTH_SECRET"]
+  provider :tradegecko, ENV["20085853f349dea3529f04591ee74a8feb17c72d3158e477fc0c959956b1b6b4"], ENV["0af3da8dd0263a7f372c44cf35250da9f02017867c34a823df00aa494193a777"]
 end
